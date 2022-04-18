1ea74d1bb202a1cd6d69f054aa214398805d6891
Thomas van der Westen <thomas@pixelpillow.nl>
2022-04-18 10:36:29 +0200

    New frontpage

--- app/components/app-header/component.ts
+++ app/components/app-header/component.ts
@@ -1,48 +1,9 @@
-import { service } from '@ember/service';
 import Component from '@glimmer/component';
-import CurrentUserService from '../../services/current-user';
-import { tracked } from '@glimmer/tracking';
-import { action } from '@ember/object';
 
 interface AppHeaderArgs {
   title: string;
   isSmall: boolean;
 }
 
-export default class AppHeader extends Component<AppHeaderArgs> {
-  @service public declare session: any;
-  @service public declare currentUser: CurrentUserService;
-
-  @tracked public isMobileMenuOpen = false;
-
-  menuItems = [
-    {
-      title: 'menu.trips',
-      route: 'authenticated.trips',
-      currentWhen: 'authenticated.trips',
-    },
-    {
-      title: 'menu.shared_trips',
-      route: 'authenticated.sharedtrips',
-      currentWhen: 'authenticated.sharedtrips',
-    },
-  ];
-
-  menuMobileItems = [
-    ...this.menuItems,
-    {
-      title: 'menu.logout',
-      route: 'logout',
-      currentWhen: 'logout',
-    },
-  ];
-
-  get mobileMenuIcon() {
-    return this.isMobileMenuOpen ? 'times' : 'bars';
-  }
-
-  @action
-  toggleMobileMenu() {
-    this.isMobileMenuOpen = !this.isMobileMenuOpen;
-  }
-}
+// eslint-disable-next-line ember/no-empty-glimmer-component-classes
+export default class AppHeader extends Component<AppHeaderArgs> {}
